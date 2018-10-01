5afa53a392a536dbb88cd9eb7812277f4a8a6428
gvergnaud <gabriel.vergnaud@gmail.com>
2018-10-01 14:33:56 +0200

    Adds sphere intersection

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "ray.h"
 
-vec3 color (const ray& r) {
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center; // vecteur vers le centre de la sphere
+    float a = dot(r.direction(), r.direction()); // === length(r.direction())) ^ 2
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b*b - 4*a*c; // nombre de fois ou r touche la sphere (pourquoi?)
+    return (discriminant > 0);
+}
+
+vec3 background(const ray& r) {
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+vec3 color(const ray& r) {
+    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+        return vec3(1, 0, 0);
+    return background(r);
 }
 
 int main() {
@@ -22,7 +37,7 @@ int main() {
 
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
-            
+
             ray r(
                 origin,
                 lower_left_corner +
---
+++ readme.md
@@ -0,0 +1,5 @@
+# raytracer in a weekend
+
+# notes
+
+- le vecteur d'un point a vers un point b est `b - a`.
