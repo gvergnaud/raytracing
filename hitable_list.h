f8a14a6bd44b46af3e13183e544f5c2487fc7b3e
gvergnaud <gabriel.vergnaud@gmail.com>
2018-10-04 00:22:30 +0200

    Add camera and antialiasing

--- hitable_list.h
+++ hitable_list.h
@@ -21,7 +21,7 @@ bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) 
     double closest_so_far = t_max;
 
     for (int i = 0; i < list_size; i++) {
-        if (list[i]->hit(r, t_min, t_max, temp_rec)) {
+        if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
             hit_anything = true;
             closest_so_far = temp_rec.t;
             rec = temp_rec;
--- raytracer.cpp
+++ raytracer.cpp
@@ -3,6 +3,7 @@
 #include "ray.h"
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 
 
 vec3 background(const ray& r) {
@@ -23,34 +24,31 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100;
+
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-
     hitable *list[2];
-
     list[0] = new sphere(vec3(0,0,-1), 0.5);
     list[1] = new sphere(vec3(0,-100.5,-1), 100);
-
     hitable *world = new hitable_list(list, 2);
+    
+    camera cam;
 
     for (int j = ny - 1; j >= 0; j--) {
-        for (int i = 0; i < nx; i ++) {
+        for (int i = 0; i < nx; i++) {
 
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
+            vec3 col(0, 0, 0);
 
-            ray r(
-                origin,
-                lower_left_corner +
-                horizontal * u +
-                vertical * v
-            );
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world);
+            }
+            
+            col /= float(ns);
 
-            vec3 col = color(r, world);
             int ir = int(255.99 * col.r());
             int ig = int(255.99 * col.g());
             int ib = int(255.99 * col.b());
